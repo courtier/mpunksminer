573079348cce71e095dc4e809b1f7cf5abe522d8
courtier <g59@tutanota.com>
2021-10-04 15:15:58 +0200

    gpu stuff

--- src/gpu.zig
+++ src/gpu.zig
@@ -47,129 +47,135 @@ const DATA_SIZE = 10000000;
 const KERNEL_SOURCE = @embedFile("keccak.cl");
 var KERNEL_SOURCE_C: [*c]const u8 = KERNEL_SOURCE;
 
-pub fn gpu(config: Config) !void {
-    _ = config;
+pub fn gpu(config: Config, range_start_p: u64) !void {
     var err: c_int = 0;
+    _ = err;
 
-    var data: [DATA_SIZE][32:0]u8 = undefined;
-    var results: [DATA_SIZE]u88 = undefined;
-    var correct: u32 = 0;
+    //setup args for miner_init
+    //constant char *bytes_prefix, constant ulong *range_start,
+    //global ulong *nonce_results, global uint *result_index
+    var bytes_prefix: [*c]const u8 = &prepareGPUBytesPrefix(config);
+    var range_start: u64 = range_start_p;
+    _ = range_start;
+    _ = bytes_prefix;
+    // var results: [DATA_SIZE]u88 = undefined;
+    // var correct: u32 = 0;
 
-    var global: usize = 0;
-    var local: usize = 0;
+    // var global: usize = 0;
+    // var local: usize = 0;
 
-    var device_id: c.cl_device_id = undefined;
-    var context: c.cl_context = undefined;
+    // var device_id: c.cl_device_id = undefined;
+    // var context: c.cl_context = undefined;
 
-    var commands: c.cl_command_queue = undefined;
-    var program: c.cl_program = undefined;
-    var kernel: c.cl_kernel = undefined;
+    // var commands: c.cl_command_queue = undefined;
+    // var program: c.cl_program = undefined;
+    // var kernel: c.cl_kernel = undefined;
 
-    var input: c.cl_mem = undefined;
-    var output: c.cl_mem = undefined;
+    // var input: c.cl_mem = undefined;
+    // var output: c.cl_mem = undefined;
 
-    var i: usize = 0;
-    var j: usize = 0;
-    const count: c_uint = DATA_SIZE;
-    while (i < count) {
-        while (j < 32) {
-            data[i][j] = random.intRangeAtMost(u8, 0, 255);
-            j += 1;
-        }
-        i += 1;
-    }
+    // var i: usize = 0;
+    // var j: usize = 0;
+    // const count: c_uint = DATA_SIZE;
+    // while (i < count) {
+    //     while (j < 32) {
+    //         data[i][j] = random.intRangeAtMost(u8, 0, 255);
+    //         j += 1;
+    //     }
+    //     i += 1;
+    // }
 
-    err = c.clGetDeviceIDs(null, c.CL_DEVICE_TYPE_GPU, 1, &device_id, null);
-    if (err != c.CL_SUCCESS) {
-        log.err("failed to create a device group. {d}", .{err});
-        os.exit(1);
-    }
+    // err = c.clGetDeviceIDs(null, c.CL_DEVICE_TYPE_GPU, 1, &device_id, null);
+    // if (err != c.CL_SUCCESS) {
+    //     log.err("failed to create a device group. {d}", .{err});
+    //     os.exit(1);
+    // }
 
-    log.info("got device id: {d}", .{device_id});
+    // log.info("got device id: {d}", .{device_id});
 
-    context = c.clCreateContext(0, 1, &device_id, null, null, &err);
-    if (context == null or err != c.CL_SUCCESS) {
-        log.err("failed to create a compute context. {d}", .{err});
-        os.exit(1);
-    }
+    // context = c.clCreateContext(0, 1, &device_id, null, null, &err);
+    // if (context == null or err != c.CL_SUCCESS) {
+    //     log.err("failed to create a compute context. {d}", .{err});
+    //     os.exit(1);
+    // }
 
-    commands = c.clCreateCommandQueue(context, device_id, 0, &err);
-    if (commands == null or err != c.CL_SUCCESS) {
-        log.err("failed to create a command queue. {d}", .{err});
-        os.exit(1);
-    }
+    // commands = c.clCreateCommandQueue(context, device_id, 0, &err);
+    // if (commands == null or err != c.CL_SUCCESS) {
+    //     log.err("failed to create a command queue. {d}", .{err});
+    //     os.exit(1);
+    // }
 
-    program = c.clCreateProgramWithSource(context, 1, &KERNEL_SOURCE_C, null, &err);
-    if (program == null or err != c.CL_SUCCESS) {
-        log.err("failed to create a compute program. {d}", .{err});
-        os.exit(1);
-    }
+    // program = c.clCreateProgramWithSource(context, 1, &KERNEL_SOURCE_C, null, &err);
+    // if (program == null or err != c.CL_SUCCESS) {
+    //     log.err("failed to create a compute program. {d}", .{err});
+    //     os.exit(1);
+    // }
 
-    err = c.clBuildProgram(program, 0, null, null, null, null);
-    if (err != c.CL_SUCCESS) {
-        log.err("failed to build the program executable. {d}", .{err});
-        var buf: [2048]u8 = undefined;
-        var len: usize = 0;
-        _ = c.clGetProgramBuildInfo(program, device_id, c.CL_PROGRAM_BUILD_LOG, 1024, &buf, &len);
-        log.err("{s}", .{buf});
-        os.exit(1);
-    }
+    // err = c.clBuildProgram(program, 0, null, null, null, null);
+    // if (err != c.CL_SUCCESS) {
+    //     log.err("failed to build the program executable. {d}", .{err});
+    //     var buf: [2048]u8 = undefined;
+    //     var len: usize = 0;
+    //     _ = c.clGetProgramBuildInfo(program, device_id, c.CL_PROGRAM_BUILD_LOG, 1024, &buf, &len);
+    //     log.err("{s}", .{buf});
+    //     os.exit(1);
+    // }
 
-    kernel = c.clCreateKernel(program, "test", &err);
-    //kernel = c.clCreateKernel(program, "square", &err);
-    if (kernel == null or err != c.CL_SUCCESS) {
-        log.err("failed to create the compute kernel. {d}", .{err});
-        os.exit(1);
-    }
+    // kernel = c.clCreateKernel(program, "test", &err);
+    // //kernel = c.clCreateKernel(program, "square", &err);
+    // if (kernel == null or err != c.CL_SUCCESS) {
+    //     log.err("failed to create the compute kernel. {d}", .{err});
+    //     os.exit(1);
+    // }
 
-    output = c.clCreateBuffer(context, c.CL_MEM_WRITE_ONLY, @sizeOf(@TypeOf(u88)) * count, null, null).?;
-    if (input == null or output == null) {
-        log.err("failed to allocate device memory. {d}", .{err});
-        os.exit(1);
-    }
+    // output = c.clCreateBuffer(context, c.CL_MEM_WRITE_ONLY, @sizeOf(@TypeOf(u88)) * count, null, null).?;
+    // if (input == null or output == null) {
+    //     log.err("failed to allocate device memory. {d}", .{err});
+    //     os.exit(1);
+    // }
 
-    err = c.clEnqueueWriteBuffer(commands, output, c.CL_TRUE, 0, @sizeOf(u8) * 32 * count, &data, 0, null, null);
-    if (err != c.CL_SUCCESS) {
-        log.err("failed to write source array. {d}", .{err});
-        os.exit(1);
-    }
+    // err = c.clEnqueueWriteBuffer(commands, output, c.CL_TRUE, 0, @sizeOf(u8) * 32 * count, &data, 0, null, null);
+    // if (err != c.CL_SUCCESS) {
+    //     log.err("failed to write source array. {d}", .{err});
+    //     os.exit(1);
+    // }
 
-    err = 0;
-    err |= c.clSetKernelArg(kernel, 1, @sizeOf(c.cl_mem), &output);
-    if (err != c.CL_SUCCESS) {
-        log.err("failed to set kernel arguments. {d}", .{err});
-        os.exit(1);
-    }
+    // err = 0;
+    // err |= c.clSetKernelArg(kernel, 1, @sizeOf(c.cl_mem), &output);
+    // if (err != c.CL_SUCCESS) {
+    //     log.err("failed to set kernel arguments. {d}", .{err});
+    //     os.exit(1);
+    // }
 
-    err = c.clGetKernelWorkGroupInfo(kernel, device_id, c.CL_KERNEL_WORK_GROUP_SIZE, @sizeOf(@TypeOf(local)), &local, null);
-    if (err != c.CL_SUCCESS) {
-        log.err("failed to retrieve kernel work group info. {d}", .{err});
-        os.exit(1);
-    }
+    // err = c.clGetKernelWorkGroupInfo(kernel, device_id, c.CL_KERNEL_WORK_GROUP_SIZE, @sizeOf(@TypeOf(local)), &local, null);
+    // if (err != c.CL_SUCCESS) {
+    //     log.err("failed to retrieve kernel work group info. {d}", .{err});
+    //     os.exit(1);
+    // }
 
-    global = count;
-    err = c.clEnqueueNDRangeKernel(commands, kernel, 1, null, &global, &local, 0, null, null);
-    if (err != c.CL_SUCCESS) {
-        log.err("failed to execute the kernel. {d}", .{err});
-        os.exit(1);
-    }
+    // global = count;
+    // err = c.clEnqueueNDRangeKernel(commands, kernel, 1, null, &global, &local, 0, null, null);
+    // if (err != c.CL_SUCCESS) {
+    //     log.err("failed to execute the kernel. {d}", .{err});
+    //     os.exit(1);
+    // }
 
-    _ = c.clFinish(commands);
+    // _ = c.clFinish(commands);
 
-    err = c.clEnqueueReadBuffer(commands, output, c.CL_TRUE, 0, @sizeOf(@TypeOf(u88)) * count, &results, 0, null, null);
-    if (err != c.CL_SUCCESS) {
-        log.err("failed to read output array. {d}", .{err});
-        os.exit(1);
-    }
+    // err = c.clEnqueueReadBuffer(commands, output, c.CL_TRUE, 0, @sizeOf(@TypeOf(u88)) * count, &results, 0, null, null);
+    // if (err != c.CL_SUCCESS) {
+    //     log.err("failed to read output array. {d}", .{err});
+    //     os.exit(1);
+    // }
 
-    correct = 0;
-    i = 0;
-    log.info("computed {d}/{d} correct values.", .{ correct, count });
+    // correct = 0;
+    // i = 0;
+    // log.info("computed {d}/{d} correct values.", .{ correct, count });
 
-    _ = c.clReleaseMemObject(input);
-    _ = c.clReleaseMemObject(output);
-    _ = c.clReleaseProgram(program);
-    _ = c.clReleaseKernel(kernel);
-    _ = c.clReleaseCommandQueue(commands);
-    _ = c.clReleaseContext(context);
+    // _ = c.clReleaseMemObject(input);
+    // _ = c.clReleaseMemObject(output);
+    // _ = c.clReleaseProgram(program);
+    // _ = c.clReleaseKernel(kernel);
+    // _ = c.clReleaseCommandQueue(commands);
+    // _ = c.clReleaseContext(context);
 }
--- src/miner.cl
+++ src/miner.cl
@@ -1,34 +1,30 @@
 // difficulty target is <64 bits, use 64 bit int
 // ulong is 64 bit
+// nonce is 8 bytes
 // randomize bytes, not nonce ??
 
-inline void encode_nonce(char *bytes, ulong nonce) {
-  // 8 bytes
-  bytes[31] = nonce & 255;
-  bytes[30] = (nonce >> 8) & 255;
-  bytes[29] = (nonce >> 16) & 255;
-  bytes[28] = (nonce >> 24) & 255;
-  bytes[27] = (nonce >> 32) & 255;
-  bytes[26] = (nonce >> 40) & 255;
-  bytes[25] = (nonce >> 48) & 255;
-  bytes[24] = (nonce >> 56) & 255;
-}
-
-kernel void miner_init(global ulong *nonces, constant char *bytes_prefix,
-                       global ulong *nonce_result, global uint *result_index,
-                       constant ulong *range_start) {
+kernel void miner_init(constant char *bytes_prefix, constant ulong *range_start,
+                       global ulong *nonce_results, global uint *result_index) {
   uint worker_id = (ulong)get_global_id(0);
-  ulong nonce = *range_start + worker_id;
+  ulong nonce = (*range_start) + worker_id;
   char local_bytes[32];
-  for (uint i = 0; i < 24; i++) {
-      local_bytes[i] = bytes_prefix[i];
+  uint i;
+  for (i = 0; i < 24; i++) {
+    local_bytes[i] = bytes_prefix[i];
   }
-  encode_nonce(local_bytes, nonce);
-  
+  local_bytes[31] = nonce & 255;
+  local_bytes[30] = (nonce >> 8) & 255;
+  local_bytes[29] = (nonce >> 16) & 255;
+  local_bytes[28] = (nonce >> 24) & 255;
+  local_bytes[27] = (nonce >> 32) & 255;
+  local_bytes[26] = (nonce >> 40) & 255;
+  local_bytes[25] = (nonce >> 48) & 255;
+  local_bytes[24] = (nonce >> 56) & 255;
+  // hash to decimal then cast to ulong by doing (number & 0xFFFFFFFF)
   // TODO
   bool valid_nonce = true;
   if (valid_nonce) {
     atomic_inc(result_index);
-    nonce_result[*result_index] = nonce;
+    nonce_results[*result_index] = nonce;
   }
 }
