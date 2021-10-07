c6aff680f202ec359b41b5d33110bca71e5549c5
courtier <g59@tutanota.com>
2021-10-07 06:58:18 +0200

    add functions for later use with a supervisor

--- src/config.zig
+++ src/config.zig
@@ -1,3 +1,6 @@
+const std = @import("std");
+const Mutex = std.Thread.Mutex;
+
 last_mined: u96 = 2475882076944016005221515264,
 //me : 0x725aEF067EeE7B1eB7B06A7404b7b65afa04193B
 //test : 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
@@ -11,3 +14,32 @@ bytes_prefix: [32]u8 = undefined,
 gpu_difficulty_target: u64 = 5731203885580,
 //TODO add option to both automatically determine this and override it manually
 gpu_work_size_max: usize = 100,
+
+last_mined_mutex: *Mutex = undefined,
+difficulty_target_mutex: *Mutex = undefined,
+
+const Self = @This();
+
+pub fn updateDifficultyTarget(self: *Self, target: u64) void {
+    const held = self.difficulty_target_mutex.acquire();
+    defer held.release();
+    self.difficulty_target = target;
+    self.gpu_difficulty_target = target;
+}
+
+pub fn getDifficultyTarget(self: *Self) u64 {
+    const held = self.difficulty_target_mutex.acquire();
+    defer held.release();
+    return self.gpu_difficulty_target;
+}
+
+pub fn updateLastMined(self: *Self, mined: u96) void {
+    const held = self.last_mined_mutex.acquire();
+    defer held.release();
+    self.last_mined = mined;
+}
+pub fn getLastMined(self: *Self) u96 {
+    const held = self.last_mined_mutex.acquire();
+    defer held.release();
+    return self.last_mined;
+}
